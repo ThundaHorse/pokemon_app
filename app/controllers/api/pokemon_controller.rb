7c2626b3c925ceab44ca0070dc7818c928da94f5
ThundaHorse <abeeeeekim@gmail.com>
2019-08-28 15:20:59 -0400

    made login/out functionality for vue app

--- app/controllers/api/pokemon_controller.rb
+++ app/controllers/api/pokemon_controller.rb
@@ -1,4 +1,6 @@
 class Api::PokemonController < ApplicationController
+  before_action :authenticate_user
+  
   def index 
     pokemons = Pokemon.all
     @pokemons = pokemons.sort_by { |poke| poke.pokemon_id }
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
     get '/pokemon' => 'pokemon#index'
     get '/pokemon/:id' => 'pokemon#show'
 
+    get '/users' => 'users#index'
     post "/users" => "users#create"
+    
     post "/sessions" => "sessions#create"
   end 
 end
