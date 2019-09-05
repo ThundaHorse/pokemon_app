faf425ab88d3927b7c53a84ae9360feddeeea041
ThundaHorse <abeeeeekim@gmail.com>
2019-09-05 14:09:49 -0400

    dropped users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_30_020618) do
+ActiveRecord::Schema.define(version: 2019_09_05_180822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,14 +38,6 @@ ActiveRecord::Schema.define(version: 2019_08_30_020618) do
     t.boolean "shiny_raid", default: false
     t.boolean "shiny_wild", default: false
     t.text "url"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
