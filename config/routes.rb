791a6436844b160e8eec4d7f5324e8f628e5b407
ThundaHorse <abeeeeekim@gmail.com>
2019-09-06 14:28:10 -0400

    deploy to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   namespace :api do 
     get '/pokemon' => 'pokemon#index'
     get '/pokemon/:id' => 'pokemon#show'
+
+    get "/*path" => proc { [200, {}, [ActionView::Base.new.render(file: 'public/index.html')]] }
   end 
 end
